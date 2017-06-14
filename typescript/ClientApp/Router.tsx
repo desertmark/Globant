d00ec3c6137099d75b2f1ba6d503f8a15181c54d
Fernando Asulay <fer.guitar.ln@gmail.com>
2017-06-13 23:46:00 -0300

    .

---
+++ typescript/ClientApp/Router.tsx
@@ -0,0 +1,21 @@
+/*import * as React from 'react';
+import {ConnectedRouter} from 'react-router-redux';
+import {Route} from 'react-router';
+import App from './apps/main/App.jsx';
+import {connect} from 'react-redux';
+
+export interface RouterType{
+    history: any
+}
+
+class Router extends React.Component<RouterType,undefined>{
+    render(){
+        return (
+            <ConnectedRouter history={this.props.history}>
+                <Route exact path="/" children={App} />
+            </ConnectedRouter>
+        );
+    }
+}
+
+export default Router;*/
